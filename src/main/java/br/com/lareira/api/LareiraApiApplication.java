edcba30ac30ec2314c31a17db6137424e5bd25ed
Rodrigo Rossi <rdrg.rossi@gmail.com>
2019-06-16 13:14:38 -0300

    Commit inicial

---
+++ src/main/java/br/com/lareira/api/LareiraApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.lareira.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LareiraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LareiraApiApplication.class, args);
+	}
+
+}
