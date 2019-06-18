b981374a10324f6f8c1b10af7b7533efc42c7286
Rodrigo Rossi <rdrg.rossi@gmail.com>
2019-06-18 09:00:16 -0300

    Criação Classes Complementares Lareira;

---
+++ src/main/java/br/com/lareira/api/model/Funcao.java
@@ -0,0 +1,70 @@
+package br.com.lareira.api.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "FUNCAO")
+public class Funcao implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Basic(optional = false)
+	@NotNull
+	@Column(name = "idfuncao")
+	private Integer idFuncao;
+
+	@Size(max = 50)
+	@Column(name = "nome")
+	private String nome;
+
+	public Integer getIdFuncao() {
+		return idFuncao;
+	}
+
+	public void setIdFuncao(Integer idFuncao) {
+		this.idFuncao = idFuncao;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((idFuncao == null) ? 0 : idFuncao.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Funcao other = (Funcao) obj;
+		if (idFuncao == null) {
+			if (other.idFuncao != null)
+				return false;
+		} else if (!idFuncao.equals(other.idFuncao))
+			return false;
+		return true;
+	}
+
+}
---
+++ src/main/java/br/com/lareira/api/model/Setor.java
@@ -0,0 +1,70 @@
+package br.com.lareira.api.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "SETOR")
+public class Setor implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Basic(optional = false)
+	@NotNull
+	@Column(name = "idsetor")
+	private Integer idSetor;
+
+	@Size(max = 50)
+	@Column(name = "nome")
+	private String nome;
+
+	public Integer getIdSetor() {
+		return idSetor;
+	}
+
+	public void setIdSetor(Integer idSetor) {
+		this.idSetor = idSetor;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((idSetor == null) ? 0 : idSetor.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Setor other = (Setor) obj;
+		if (idSetor == null) {
+			if (other.idSetor != null)
+				return false;
+		} else if (!idSetor.equals(other.idSetor))
+			return false;
+		return true;
+	}
+
+}
